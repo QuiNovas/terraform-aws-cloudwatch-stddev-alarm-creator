66e03c27754c1e8925df61dce7c4dbe6678e624a
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-05-31 16:47:48 -0500

    name too long

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  creator_name = "cloudwatch-stddev-alarm-creator-${random_uuid.name_suffix.result}"
+  creator_name = "cloudwatch-stddev-alarm-creator-${random_string.name_suffix.result}"
 }
